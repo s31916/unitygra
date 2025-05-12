9452a2c953e85d9f7e22eabe28dd25e62522559c
s31916 <s31916@pjwstk.edu.pl>
2025-05-12 15:36:33 +0200

    gowno
    gtrgtr

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,3 +1,4 @@
+﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,15 +8,59 @@ public class GameController : MonoBehaviour
 {
     public string mainWord = "default";
     public TextMeshProUGUI mainWordText;
+    public TextMeshProUGUI livesCountText;
+    public TextMeshProUGUI messageText;
+    public TMP_InputField inputField;
+    public int livesCount = 10;
     // Start is called before the first frame update
     void Start()
     {
         mainWordText.text = mainWord;
+        livesCountText.text = $"Zostało {livesCount} prób";
     }
+
+    public void OnClick()
+
+    {
+        Debug.Log($"Guzik zostal klikniety");
+        livesCount = livesCount - 1;
+        livesCountText.text = $"Zostało {livesCount} prób";
+
+        if (mainWord == inputField.text)
+        {
+            messageText.text = $"Poprawne słowo zostało wpisane";
+            return;
+        }
+        if (mainWord.Length != inputField.text.Length)
+        {
+            messageText.text = $"Niepoprawne słowo zostało wpisane- zła liczba liter";
+            return;
+        }
+        int bullCount = CountBulls();
+        int cowCount = CountCows();
+        messageText.text = $"Bulls: {bullCount} and Cows: {cowCount}";
+
+    }
+    public int CountBulls()
+    {
+        int result = 0;
+        for (int i = 0; i< mainWord.Length; i++)
+        {
+            if (mainWord[i] != inputField.text[i] && mainWord.Contains(inputField.text[i]) )
+                
+                    
+                    }
+
+        return 0; }
+
+    public int CountCows()
+    { return 0; }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
